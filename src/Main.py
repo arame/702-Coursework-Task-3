3c7304333c7ade7480922f397a73ae02b4c7e4d1
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2019-11-18 20:14:57 +0000

    Initial part 4

--- src/Main.py
+++ src/Main.py
@@ -24,7 +24,7 @@ train_labels = data[2]
 test_labels = data[3]
 train_labels_one_hot = data[4]
 test_labels_one_hot = data[5]
-image_size = 28 # width and length
+image_size = 28             # width and length
 no_of_different_labels = 10 #  i.e. 0, 1, 2, 3, ..., 9
 image_pixels = image_size * image_size
 
