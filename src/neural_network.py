Another failed attempt to fix the bug to do with softmax

src/neural_network.py
@@ -62,13 +62,13 @@ class NeuralNetwork:
         loss = Cross_Entropy.calc(output_network, target_vector)
         
         gradient = Cross_Entropy.derived_calc(output_hidden, target_vector)
+        tmp1 = loss * gradient
         # update the weights:
         derived1 = Derivative.reLU(gradient)
-        tmp = loss * derived1     
-        tmp = self.learning_rate  * np.dot(tmp, gradient.T)
-
+        tmp2 = derived1 * tmp1
+        tmp3 = self.learning_rate  * np.dot(tmp2, output_hidden.T)
         # TODO - fix this bug, exception caused by dimensions
-        self.weights_hidden_output += tmp
+        self.weights_hidden_output += tmp3
         # calculate hidden errors:
         hidden_errors = np.dot(self.weights_hidden_output.T, loss)
         # ----------------------------------------------------------------------

src/softmax.py
@@ -6,5 +6,8 @@ class Softmax:
         sum_exp = np.sum(np.exp(x), axis=0)
         if sum_exp == 0:
             sum_exp = 0.0001  # Make sure division not by zero
-        smax = np.exp(x)/sum_exp
-        return smax     # returns a vector of the same shape as x
+        #smax = np.exp(x)/sum_exp
+        #return smax     # returns a vector of the same shape as x
+        e_x = np.exp(x - np.max(x))
+        softmax = e_x / e_x.sum(axis=0)
+        return softmax